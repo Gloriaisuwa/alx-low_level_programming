08cd923e477985e90b0328efcdb022fdfa7ab88d
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-09-28 13:48:11 -0700

    You mustn't be afraid to dream a little bigger, darling

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -7,14 +7,15 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
-	{
-		return (-1);
-	}
-
-	if (n == 0)
+	if ((n == 1) || (n == 0))
 	{
 		return (1);
 	}
-	return(n * factorial (n - 1));
+
+	if (n >= 0)
+	{
+		return (n * (factorial(n - 1)));
+	}
+	else
+		return (-1);
 }
