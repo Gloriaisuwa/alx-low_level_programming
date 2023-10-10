f56852134ad699867f5d2469b941d01f83916751
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-10-10 15:08:12 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,11 +9,16 @@
  * Description: struct called "dog" that stores its name, its age
  * and the name of its owner
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
