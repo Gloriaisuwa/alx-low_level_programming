e09ae59565f0d1ad7c99d645e852375d80287625
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-10-26 06:05:10 -0700

    convert binary to integer

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,8 +18,10 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (len = 0; b[len] != '\0'; len++)
 		;
-	if (len == 1 && ( b[0] == '0' || b[0] == '1'))
-			return (b[0] - 48);
+	if (len == 1 && (b[0] == '0' || b[0] == '1'))
+	{
+		return (b[0] - 48);
+	}
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
