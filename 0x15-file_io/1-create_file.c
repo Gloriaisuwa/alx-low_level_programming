65616b8d9b18a69809a32a07cc18dc36d2ec9c35
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-10-30 07:15:35 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
  * create_file - creates a file
  * @filename: filename
  * @text_content: content writed in a file
- * Return: 1 if it's success or -1 if it fails
+ * Return: 1 on success or -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
@@ -14,21 +14,20 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR, S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content == NULL)
 		text_content = "";
+
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
-
 	rwr = write(fd, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
-
 	close(fd);
 
 	return (1);
