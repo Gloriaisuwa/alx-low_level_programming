e8750b1081b84fa9d3cba081034fea0e6c632d7c
Gloriaisuwa <gloriaisuwa@gmail.com>
2023-12-13 05:48:15 -0800

    hack for jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p .. https://raw.githubusercontent.com/Gloriaisuwa/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
-export LD_PRELOAD="$PWD/../libhack.so"
+wget -p /tmp https://github.com/Gloriaisuwa/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD=/tmp/libhack.so
